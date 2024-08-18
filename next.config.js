f04b19b30124701153246fea6c08008f6df3f114
o <3vefaaaa@mail.ru>
2024-08-18 19:30:55 +0700

    и опять

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 
 const nextConfig = {
-    API_URL: "http://62.113.106.139:5000/",
+    API_URL: "http://62.113.99.62:5000/",
     filterByTypeStockValue: "Тип учреждения",
     filterByNosologicalStockValue: "Нозологическая группа",
     nosologicalGroup: {
