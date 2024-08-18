360df2fc3a7686844c36447de67f610add22ad8b
naghti <57380294+naghti@users.noreply.github.com>
2024-08-18 20:15:59 +0700

    Update next.config.js
    cdscdscds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 
 const nextConfig = {
-    API_URL: "http://localhost:5000/",
+    API_URL: "http://62.113.99.62:5000/",
     filterByTypeStockValue: "Тип учреждения",
     filterByNosologicalStockValue: "Нозологическая группа",
     nosologicalGroup: {
