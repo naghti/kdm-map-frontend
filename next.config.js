0f0324809208bfeb1599f5dfa1d156d5a0e7af07
o <3vefaaaa@mail.ru>
2024-08-19 16:09:11 +0700

    env file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 
 const nextConfig = {
-    API_URL: "http://62.113.99.62:5000/",
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
     filterByTypeStockValue: "Тип учреждения",
     filterByNosologicalStockValue: "Нозологическая группа",
     nosologicalGroup: {
-        "invalid": "Маломобильных граждан",
-        "bleed": "Инвалидов по зрению",
-        "ear": "Инвалидов по слуху",
+        "invalid": "Маломобильные граждане",
+        "bleed": "Инвалиды по зрению",
+        "ear": "Инвалиды по слуху",
     }
 }
 
