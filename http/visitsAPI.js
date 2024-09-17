ff2c572857e4d0706122df22c89a02dc22ccf890
o <3vefaaaa@mail.ru>
2024-09-17 23:42:32 +0700

    bug fix

--- http/visitsAPI.js
+++ http/visitsAPI.js
@@ -2,5 +2,5 @@ import {$host} from "./index";
 
 export const getVisits = async (req, res) => {
     const {data} = await $host.get("api/visitors")
-    return data[0].amount
+    return data.amount
 }
